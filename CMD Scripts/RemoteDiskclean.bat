426eda98f046323557315bcfc74185835e3f5e69
Max Kulik <pearelectronics@live.com>
2023-11-15 15:10:16 -0600

    Create RemoteDiskclean.bat
    Added A remote disk clean script. This can be used on a remote shell/cmd to an endpoint you are managing.

---
+++ CMD Scripts/RemoteDiskclean.bat
@@ -0,0 +1,4 @@
+rem This oneliner will auto run Disk Cleanup and try to solve for a very low space disk on it's own. 
+rem It will also prompt the user when it's finished as well. 
+
+cleanmgr.exe /VERYLOWDISK
